c27ad723b5176f997ccc4cb089256736562c17af
guyi <love-jia-jun@outlook.com>
2020-05-08 09:04:38 +0800

    取出版本号中的快照标识

--- compile-lib/src/main/java/top/guyi/iot/ipojo/compile/lib/expand/manifest/defaults/BaseManifestExpand.java
+++ compile-lib/src/main/java/top/guyi/iot/ipojo/compile/lib/expand/manifest/defaults/BaseManifestExpand.java
@@ -12,12 +12,14 @@ public class BaseManifestExpand implements ManifestExpand {
 
     @Override
     public List<Manifest> execute(ClassPool pool, Set<CompileClass> components, Compile compile) {
+        String version = compile.getProject().getVersion();
+        version = version.endsWith("-SNAPSHOT") ? version.replace("-SNAPSHOT","") : version;
         return Arrays.asList(
                 new Manifest("Manifest-Version","1.0"),
                 new Manifest("Bundle-ManifestVersion","2"),
                 new Manifest("Bundle-Name", compile.getName()),
                 new Manifest("Bundle-SymbolicName",compile.getSymbolicName()),
-                new Manifest("Bundle-Version",compile.getProject().getVersion()),
+                new Manifest("Bundle-Version", version),
                 new Manifest("Private-Package",compile.getPackageName())
         );
     }
